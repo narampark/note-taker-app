d15ee713550d788ed111143f7bf1a24cf40715e7
GodSlappedMe <narampark92@gmail.com>
2023-11-09 17:44:29 -0500

    server.js added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const apiRoutes = require("./routes/apiRoutes");
+app.use(apiRoutes);
+const htmlRoutes = require("./routes/htmlRoutes");
+app.use(htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
