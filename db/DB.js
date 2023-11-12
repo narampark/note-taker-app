674fffdcc540faa0b212b85413cc8b11d09b9a17
GodSlappedMe <narampark92@gmail.com>
2023-11-12 17:18:50 -0500

    changed uuid to older version

--- db/DB.js
+++ db/DB.js
@@ -5,34 +5,36 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class DB {
-    async readNotes() {
-        try {
-            const notesRaw = await readFileAsync(noteData, "UTF8");
-            return notesRaw ? JSON.parse(notesRaw) : [];
-        } catch (error) {
-            throw error;
-        }
+  async readNotes() {
+    try {
+      const notesRaw = await readFileAsync(noteData, "UTF8");
+      return notesRaw ? JSON.parse(notesRaw) : [];
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async addNote(data) {
-        try {
-            await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
-                console.log("new note added");
-            });
-        } catch (error) {
-            throw error;
+  async addNote(data) {
+    try {
+      await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(
+        () => {
+          console.log("new note added");
         }
+      );
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async deleteNote(data) {
-        try {
-            await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
-                console.log("note deleted");
-            });
-        } catch (error) {
-            throw error;
-        }
-    }
+  async deleteNote(data) {
+      try {
+          await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
+              console.log("note deleted");
+          });
+      } catch (error) {
+          throw error;
+      }
+  }
 }
 
 module.exports = new DB();
